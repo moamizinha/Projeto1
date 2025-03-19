0bb28789f0eaa4cdc01cd24613d74c0c2a7722a8
Michele Rodrigues Hempel Lima <michele.rodrigues@feinet.fei.edu.br>
2025-03-19 15:01:48 -0300

    Criando a funcao criarlista

--- projeto.c
+++ projeto.c
@@ -2,8 +2,23 @@
 #include "stdio.h"
 
 int criarTarefa(ListaDeTarefas *lt){
-    printf("criar tarefa\n");
-    return 0;}
+ if(lt->qtd >= TOTAL_TAREFAS)
+ return 1;
+
+ Tarefa *t = &lt -> tarefas[lt->qtd];
+ 
+    printf("Entre com a prioridade da tarefa:");
+    scanf("%d", & t->prioridade);
+
+    printf("Entre com a catergoria da tarefa:");
+    scanf("%s", & t->categoria);
+
+    printf("Entre com a catergoria da tarefa:");
+    scanf("%s", & t->descricao);
+
+lt -> qtd++;
+return 0;}
+
 int deletarTarefa(ListaDeTarefas *lt){
     printf("deletar tarefa\n");
     return 0;}
